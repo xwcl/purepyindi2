63976eac5840422dda28beabe698afd3747db09e
Joseph Long <github@joseph-long.com>
2023-03-01 11:57:38 +0000

    Hook up broadcasting in server

--- purepyindi2/server.py
+++ purepyindi2/server.py
@@ -10,9 +10,10 @@ class IndiServerClient:
     connection : IndiTcpServerConnection
     interested_properties : set
 
-    def __init__(self, connection, client_socket):
+    def __init__(self, connection, handle_inbound):
         self.connection = connection
-        self.connection.start(client_socket)
+        self.interested_properties = set()
+        self.handle_inbound = handle_inbound
 
     def handle_client_to_server_message(self, message):
         if not isinstance(message, messages.GetProperties):
@@ -20,6 +21,7 @@ class IndiServerClient:
         device_name = message.device if message.device is not None else ALL
         property_name = message.name if message.name is not None else ALL
         self.interested_properties.add((device_name, property_name))
+        self.handle_inbound(message)
 
     def handle_server_to_client_message(self, message):
         if not isinstance(message, messages.IndiDefSetDelMessage):
@@ -27,9 +29,9 @@ class IndiServerClient:
         device_name = message.device
         property_name = message.name
         interested = (
-            (ALL, ALL) in self._interested_properties or
-            (device_name, ALL) in self._interested_properties or
-            (device_name, property_name) in self._interested_properties
+            (ALL, ALL) in self.interested_properties or
+            (device_name, ALL) in self.interested_properties or
+            (device_name, property_name) in self.interested_properties
         )
         if interested:
             self.connection.send(message)
@@ -53,8 +55,6 @@ class IndiServer:
         for remote_host, remote_port in remote_servers:
             c = IndiTcpClientConnection(host=remote_host, port=remote_port)
             c.start()
-            msg = messages.GetProperties()
-            c.send(msg)
             self.remote_server_clients[(remote_host, remote_port)] = c
             c.add_callback(TransportEvent.inbound, self.broadcast)
 
@@ -64,14 +64,20 @@ class IndiServer:
     def accept_connection(self, client_socket, client_host, client_port):
         conn = IndiTcpServerConnection(host=client_host, port=client_port)
         conn.start(client_socket)
-        self.clients[(client_host, client_port)] = conn
+        c = IndiServerClient(conn, self.forward)
+        self.clients[(client_host, client_port)] = c
         conn.add_callback(TransportEvent.connection, partial(self.client_status, client_key=(client_host, client_port)))
+        conn.add_callback(TransportEvent.inbound, c.handle_client_to_server_message)
 
-    def client_status(self, event : TransportEvent, status : ConnectionStatus, client_key):
+    def client_status(self, status : ConnectionStatus, client_key):
         if status is not ConnectionStatus.CONNECTED:
             del self.clients[client_key]
 
     def broadcast(self, indi_action):
         # TODO rewrite to r/o, filter non-visible
         for client_key in self.clients:
-            self.clients[client_key].send(indi_action)
+            self.clients[client_key].handle_server_to_client_message(indi_action)
+
+    def forward(self, indi_action):
+        for remote in self.remote_server_clients.values():
+            remote.send(indi_action)
